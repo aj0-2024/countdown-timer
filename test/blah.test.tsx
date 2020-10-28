f91ee08e06ce5f3eb3339a7257e414358f7b1a83
Anirudh Janga <j.anirudh@icloud.com>
2020-10-27 22:15:11 -0500

    Added postcss

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Default as Thing } from '../stories/Thing.stories';
+import { CountdownTimer } from '../dist';
 
 describe('Thing', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
-    ReactDOM.unmountComponentAtNode(div);
-  });
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<Thing />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
 });
