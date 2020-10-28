01fa9b93dfb53c291d4f4efb6cd0c75c06be26c9
Anirudh Janga <j.anirudh@icloud.com>
2020-10-27 22:08:18 -0500

    rename count down timer component

--- stories/CountdownTimer.stories.tsx
+++ stories/CountdownTimer.stories.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react';
-import { Thing, Props } from '../src';
+import { CountdownTimerProps, CountdownTimer } from '../src';
 
 const meta: Meta = {
-  title: 'Welcome',
-  component: Thing,
-  argTypes: {
-    children: {
-      control: {
-        type: 'text',
-      },
-    },
-  },
-  parameters: {
-    controls: { expanded: true },
-  },
+    title: 'Welcome',
+    component: CountdownTimer,
 };
 
 export default meta;
 
-const Template: Story<Props> = args => <Thing {...args} />;
+const Template: Story<CountdownTimerProps> = args => (
+    <CountdownTimer {...args} />
+);
 
 // By passing using the Args format for exported stories, you can control the props for a component for reuse in a test
 // https://storybook.js.org/docs/react/workflows/unit-testing
