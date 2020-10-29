b3a9a44f696a949fdaf7f92f4a231150c50d12ed
Anirudh Janga <j.anirudh@icloud.com>
2020-10-28 21:53:15 -0500

    adding css

--- src/CountdownTimer.tsx
+++ src/CountdownTimer.tsx
@@ -1,11 +1,14 @@
-import React, { FC, HTMLAttributes } from 'react';
-import './CountdownTimer.css';
+import React, { FC, HTMLAttributes } from "react";
+import "./CountdownTimer.css";
 
-export interface CountdownTimerProps extends HTMLAttributes<HTMLDivElement> {}
+export interface CountdownTimerProps extends HTMLAttributes<HTMLDivElement> {
+    onStop?: () => void;
+    onReset?: () => void;
+}
 
 // Please do not use types off of a default export module or else Storybook Docs will suffer.
 // see: https://github.com/storybookjs/storybook/issues/9556
 
-export const CountdownTimer: FC<CountdownTimerProps> = () => {
+export const CountdownTimer: FC<CountdownTimerProps> = props => {
     return <div />;
 };
