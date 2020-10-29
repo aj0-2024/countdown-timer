d50bc9169f7e3689fc9f11af9b81c398ba1dbb52
Anirudh Janga <j.anirudh@icloud.com>
2020-10-28 21:39:13 -0500

    Removed scaffolding comments

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,7 @@ export interface CountdownTimerProps extends HTMLAttributes<HTMLDivElement> {}
 
 // Please do not use types off of a default export module or else Storybook Docs will suffer.
 // see: https://github.com/storybookjs/storybook/issues/9556
-/**
- * A custom Thing component. Neat!
- */
+
 export const CountdownTimer: FC<CountdownTimerProps> = () => {
     return <div />;
 };
