9a4ed43f7bffb7c342000f0fc9db45de733f9db4
Anirudh Janga <j.anirudh@icloud.com>
2020-11-01 21:34:22 -0600

    use double quotes for consistency.

--- test/CountdownTimer.test.tsx
+++ test/CountdownTimer.test.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import * as ReactDOM from 'react-dom';
-import { CountdownTimer } from '../dist';
+import React from "react";
+import * as ReactDOM from "react-dom";
+import { CountdownTimer } from "../dist";
 
-describe('Thing', () => {
-    it('renders without crashing', () => {
-        const div = document.createElement('div');
+describe("Thing", () => {
+    it("renders without crashing", () => {
+        const div = document.createElement("div");
         ReactDOM.render(<CountdownTimer />, div);
         ReactDOM.unmountComponentAtNode(div);
     });
